44e566e978328fc7c9e6182371d2a0c13fbd11e2
Malick-Tammal <malick-tammal15@gmail.com>
2025-05-26 21:08:56 +0100

    Editing telescope plugin

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,16 +1,21 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
 	event = "VeryLazy",
 	lazy = true,
+
+	-- HACK: Config
 	config = function()
-		require("telescope").setup({
-			pickers = {
-				find_files = {
-					theme = "ivy",
-				},
+		local tele = require("telescope")
+		tele.setup({
+			extensions = {
+				fzf = {},
 			},
 		})
+		tele.load_extension("fzf")
 	end,
 }
