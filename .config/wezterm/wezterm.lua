3c94e1505de332c85683d0bd6a4a6c0b33d02ff1
Malick-Tammal <malick-tammal15@gmail.com>
2025-05-30 14:24:09 +0100

    Fixing wezterm performence issue with neovim :) setting it to be my main term

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,57 +1,58 @@
 local wezterm = require("wezterm")
 local action = wezterm.action
-local config = {}
+local config = {
+	-- INFO: Theme
+	color_scheme = "Everforest Dark Hard (Gogh)",
 
--- INFO: Theme
-config.color_scheme = "Everforest Dark Hard (Gogh)"
+	-- INFO: Window
+	window_decorations = "INTEGRATED_BUTTONS",
+	hide_tab_bar_if_only_one_tab = true,
+	window_padding = {
+		left = 20,
+		right = 0,
+		top = 80,
+		bottom = 0,
+	},
+	window_background_opacity = 0.65,
+	macos_window_background_blur = 30,
 
--- INFO: Window
-config.window_decorations = "INTEGRATED_BUTTONS"
-config.hide_tab_bar_if_only_one_tab = true
-config.window_padding = {
-	left = 20,
-	right = 0,
-	top = 80,
-	bottom = 0,
-}
-config.window_background_opacity = 0.65
-config.macos_window_background_blur = 30
-
--- INFO: Font
-config.font = wezterm.font("MesloLGL Nerd Font", { weight = "Bold" })
-config.font_size = 15
-config.window_frame = {
+	-- INFO: Font
 	font = wezterm.font("MesloLGL Nerd Font", { weight = "Bold" }),
-}
-
--- INFO: Keybinds
-config.keys = {
-	{
-		key = "T",
-		mods = "CMD",
-		action = action.EmitEvent("toggle-opacity"),
+	font_size = 15,
+	window_frame = {
+		font = wezterm.font("MesloLGL Nerd Font", { weight = "Bold" }),
 	},
-	{
-		key = "O",
-		mods = "CMD",
-		action = action.SpawnCommandInNewWindow({
-			args = { os.getenv("SHELL"), "-c", "open ." },
-		}),
+
+	-- INFO: Keybinds
+	keys = {
+		{
+			key = "T",
+			mods = "CMD",
+			action = action.EmitEvent("toggle-opacity"),
+		},
+		{
+			key = "O",
+			mods = "CMD",
+			action = action.SpawnCommandInNewWindow({
+				args = { os.getenv("SHELL"), "-c", "open ." },
+			}),
+		},
 	},
+	-- Toggle opacity function
+	wezterm.on("toggle-opacity", function(window, pane)
+		local overrides = window:get_config_overrides() or {}
+		if not overrides.window_background_opacity then
+			overrides.window_background_opacity = 1.0
+		else
+			overrides.window_background_opacity = nil
+		end
+		window:set_config_overrides(overrides)
+	end),
+
+	-- INFO: Performence
+	front_end = "OpenGL",
+	animation_fps = 250,
+	max_fps = 250,
 }
-
--- Toggle opacity function
-wezterm.on("toggle-opacity", function(window, pane)
-	local overrides = window:get_config_overrides() or {}
-	if not overrides.window_background_opacity then
-		overrides.window_background_opacity = 1.0
-	else
-		overrides.window_background_opacity = nil
-	end
-	window:set_config_overrides(overrides)
-end)
-
-config.animation_fps = 60
-config.max_fps = 60
 
 return config
